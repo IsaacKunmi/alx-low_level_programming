a060108ec2fca47c21b4d6cf112a220c17700286
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-08 13:04:14 -0700

    Size of Types in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,11 @@ int main(void)
 	float floatType;
 	char charType;
 	double doubleType;
+	long long longType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
