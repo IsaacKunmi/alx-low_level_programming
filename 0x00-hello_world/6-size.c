66c69b48684b5bb3e2fb4bbd598c936712c8ca1f
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-08 10:49:24 -0700

    Print Size of Type in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 4 byte(s)\nSize of a float: 4 byte(s)");
+
+	return (0);
+}
