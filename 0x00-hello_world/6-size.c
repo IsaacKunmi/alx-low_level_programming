7fb4c27ec7b74aaaec4df7de6cf09c7ab9a5408b
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-08 12:21:05 -0700

    Print size of typees in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,11 @@
 
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 4 byte(s)\nSize of a float: 4 byte(s)");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 4 byte(s)\n");
+	printf("Size of a float: 4 byte(s)");
 
 	return (0);
 }
