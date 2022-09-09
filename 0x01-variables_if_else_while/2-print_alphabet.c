e359ad916e4ac7e9d5fe276f79906f3a42cbef4e
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-09 14:53:37 -0700

    Using putchar in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'C'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+	putchar(x);
+	}
+
+	return (0);
+
+}
