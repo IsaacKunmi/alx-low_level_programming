d5ec1f58154ec84c2f049454298e4d5dbf9fce8c
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-09 13:51:48 -0700

    Print last value of variable in C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,6 +17,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d\n", n);
+
+	if (n > 5)
+	{
+		printf("The last of n is %d and is greater than 5\n", n);
+	} else if (n == 0)
+	{
+		printf("Last digit of n is %d and is 0\n", n);
+	} else
+	{
+		printf("Last digit of n is %d and is less thab 6 and not 0\n", n);
+	}
+
 	return (0);
 }
