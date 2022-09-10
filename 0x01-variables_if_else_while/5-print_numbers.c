d3d822869be4d871b6d6dbeb80f6533212091e3a
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-09 17:08:32 -0700

    Print numbers Use loops in C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'C'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 10)
+	{
+	printf("%d", x);
+	x++;
+	}
+
+
+
+
+	putchar('\n');
+
+	return (0);
+
+}
