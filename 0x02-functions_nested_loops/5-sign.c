938fdaf004e1703e7f94f2a77095d66f7dc8b322
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-13 15:23:34 -0700

    Is thos pos, neg or zero in C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *print_sign - Entry
+ *
+ * @n: type int
+ *
+ * Description: checks for lowercase
+ *
+ * Return: 1 if positive or 0 for negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}	
+	else
+	{
+		_putchar(45);
+		retrun(-1);
+	}
+
+	
+}
+
