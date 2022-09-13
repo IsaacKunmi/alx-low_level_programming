57e59048d34dd35cf49591cfc39d5820a35307a4
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-13 14:43:36 -0700

    Header files 10x alphabet in C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry
+ *
+ * Description: alphabets in lowercase
+ *
+ * Return: void
+ */
+
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int y = 0;
+
+	while (y <= 10)
+	{
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar(10);
+	y++;
+	}
+
+	return;
+
+}
+
