bf26a04a4dbbb2edba4af51c58354dbdc4c9fc6a
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-13 10:34:06 -0700

    print lowercase letters using custom Header files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ *
+ * Description: Header files
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar(10);
+
+	return (0);
+
+}
+
