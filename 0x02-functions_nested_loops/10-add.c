be0d54bf25dfa2649a335fef9a6a974c26683ac5
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-13 18:08:52 -0700

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ *
+ * @i: type int
+ * @j: type int
+ * Return: Sum of numbers
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = (i + j);
+
+	return (k);
+
+}
