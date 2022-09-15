b78a97fd723a721b8aeb2fc4d41978a4a051836c
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-15 15:57:19 -0700

    Prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,16 +7,22 @@
  *Return: void
  */
 
+
 void print_square(int size)
 {
-int x, y;
+	int x, y;
 
-	for (x = 1; x <= size; x++)
+	if (size <= 0)
+		_putchar('\n');
+	else
 	{
-	for (y = 1; y <= size; y++)
-	{
-		_putchar(35);
-	}
-	_putchar("\n");
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
