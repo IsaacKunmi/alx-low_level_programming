f54def035e76aaebe603f669860d5106a11f39d1
IsaacKunmi <kunmiolamiju@gmail.com>
2022-09-19 21:16:54 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *rev_string - print strings in reverse
+ *
+ * @s: char parameter pointer type
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	 int i, j, k;
+	char h;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	k = i;
+	for (i--, j = 0; j < k / 2; i--, j++)
+	{
+		h = s[j];
+		s[j] = s[i];
+		s[i] = h;
+}
