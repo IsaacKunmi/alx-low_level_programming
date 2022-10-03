43720d8e148e2d415f6b9a1e5567f94940e2f46d
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-03 15:48:24 -0700

    Length of a substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_strspn - entry
+ *
+ * Description: gets the length of a prefix substring
+ *
+ * @s: string to check
+ *
+ * @accept: input string
+ *
+ * Return: number od bytes in sthat has bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == *accept)
+		{
+			i += 1;
+			return (i);
+		}
+
+		s++;
+	}
+
+	return (0);
+}
