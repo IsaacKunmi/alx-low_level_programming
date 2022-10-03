85a71fff8d9460a44fb0587602e2638ba4bc37b8
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-03 15:17:20 -0700

    Proto strchr code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,32 +5,28 @@
  *
  * Description: locates a character in a string
  *
- * @s: 
+ * @s: string to check
  *
- * @c:
+ * @c:charactteer to find
  *
- * Return: pointer to dest
+ * Return: char
  */
 
-char *_strchr(char *s, char c) 
+char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
 
-	char *z;
-
-	while( *(s + i) != '\0')
+	while (*s != '\0')
 	{
-		
-		if(*(s + i) == c)
-			return *(s + i);
-		else
-			return NULL;
+		if (*s == c)
+		{
+			return (s);
+		}
 
-
-		i++;
+		s++;
 	}
 
-	return (s + 1);
+	return (NULL);
+
 
 }
 
