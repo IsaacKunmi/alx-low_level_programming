ed97b4a804dfe42721ca26375b1d992a879d5a8f
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-03 21:06:03 -0700

    Locate substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *_strstr - entry
+ *
+ * Description: locates a substring
+ *
+ * @haystack: string to check
+ *
+ * @needle: substring to find
+ *
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	unsigned int i = 0;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			return (haystack);
+		}
+
+		haystack++;
+	}
+
+	return (NULL);
+
+}
