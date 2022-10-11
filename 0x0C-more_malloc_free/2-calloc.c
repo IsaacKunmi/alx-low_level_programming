b4b991fbdd89fcf6b30055119dd92fc29479a588
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-11 07:07:39 -0700

    Implement calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_calloc - allocates memory for an array
+ *@nmemb: elemements in array
+ *@size: byte size of elements
+ *Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *a;
+
+	a = calloc(size, sizeof(nmemb);
+		
+		return (a);
+}
