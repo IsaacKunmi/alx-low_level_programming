8a26ec7249445ada359eedce7f6aeb3b76b21610
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-11 18:22:09 -0700

    Create new dog using struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ *new_dog - function creates a new dog
+ *@name:
+ *@age:
+ *@owner:
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *doggy;
+
+	if (doggy != NULL)
+	{
+	new_dog.name = name;
+	new_dog.age = age;
+	new_dog.owner = owner;
+	}
+
+	return (doggy);
+}
