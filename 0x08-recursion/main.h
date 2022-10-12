b07cc38ce74a6ea410a31c1d9b44a8723517ef1f
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-12 01:42:47 -0700

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-int factorial(int n):
+int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int get_square(int c, int i);
