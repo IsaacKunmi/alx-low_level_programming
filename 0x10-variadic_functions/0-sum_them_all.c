f40e9b55bb7771a745fc9a63793304f05daf8d80
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-13 14:10:53 -0700

    Sum parameters of a variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *sum_them_all - sums all its parameters
+ *
+ *@n: initial integer for initialization of variadic function
+ *Return: Sum of parameters or 0if unsuccessful
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int a, sum;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+
+	sum = 0;
+
+	for (a = 0; a < n; n++)
+		sum += va_arg(ap, end);
+
+	va_end(ap);
+
+	return (sum);
+
+}
