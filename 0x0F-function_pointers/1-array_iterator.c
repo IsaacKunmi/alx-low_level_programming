ca3887ee2372835f1f7db9050b15c4f90b89108c
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-12 20:21:10 -0700

    Uisng function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - executes a function given as a parameter
+ *on each element of array
+ *@array: to iterate over
+ *@size: size of array
+ *@action: pointer to the function
+ */
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int a;
+
+	for (a = 0; a < size; a++)
+	{
+		action(array[a]);
+	}
+}
