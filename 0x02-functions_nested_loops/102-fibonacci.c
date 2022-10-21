486549a8689701071b8899ab2d581d8cc7181b7e
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-21 05:57:41 -0700

    Learning Nested loops in C

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: printt the first 50 fibonacci numbers starting from 1
+ *Return: 0
+ */
+
+int main(void)
+{
+
+	int count = 1;
+	long int i = 1, j = 2;
+	long int k;
+
+	printf("%lu, ", i);
+
+	while  (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu\n", j);
+		}
+		else
+		{
+			printf("%lu, ", j);
+		}
+
+		k = j;
+		j += i;
+		i = k;
+
+		count++;
+	}
+	return (0);
+}
