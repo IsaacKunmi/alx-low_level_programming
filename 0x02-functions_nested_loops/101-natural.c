cfd45d419b49e1a73e710f144749c2651e81fb9b
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-21 05:36:08 -0700

    Learning Nested loops in C

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: print the sum of all multiples of 3 or 5 below 1024
+ *Return: 0
+ */
+
+int main(void)
+{
+
+	int a = 1;
+	int sum = 0;
+
+	while (a < 1024)
+	{
+		if (a % 3 == 0)
+		{
+			sum += a;
+		} else if (a % 5)
+		{
+			sum += a;
+		}
+		a++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
