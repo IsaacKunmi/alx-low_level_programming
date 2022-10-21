20f4948e3dab71472a8619a269352569d9ddbe30
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-21 06:15:38 -0700

    Learning Nested loops in C

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: print the sumof even fibonacci numbers
+ *Return: 0
+ */
+
+int main(void)
+{
+
+	int total = 0;
+	int i = 1, j = 2;
+	int k;
+
+	while  (j < 4000000)
+	{
+		if (j % 2 == 0)
+		total += j;
+
+		k = j;
+		j += i;
+		i = k;
+
+		j++;
+	}
+
+	printf("%d\n", total);
+
+	return (0);
+}
