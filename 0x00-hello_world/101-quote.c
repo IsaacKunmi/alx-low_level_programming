e7e6712322e04f660b7bb285b061f258048c0390
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-21 03:27:18 -0700

    Introduction to C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+
+/**
+ * main - entry point
+ *
+ * Description - prints text without using printf or puts
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	char print[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	int length = strlen(print);
+
+	write(2, print, length);
+
+	return (1);
+}
