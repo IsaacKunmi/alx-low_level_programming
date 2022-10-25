eb57b6909a124f70dc879f2ebc2d880105f00cbc
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-25 10:46:09 -0700

    List elements in a struct

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_listint - entry
  * Description: prints all the elements of a listint_t list
@@ -12,7 +13,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
