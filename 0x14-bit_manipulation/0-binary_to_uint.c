a5d31ce8b98b42652ea2a723061da3fb54ff88b8
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-27 17:14:46 -0700

    Convert binary to decimal using bit operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int a = atoi(b);
+	int a = atoi(b), c;
 	int j, k, l;
 	int m = 0, sum = 0;
+
+	for (c = 0; b[c]; c++)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+	}
 
 	while (a)
 	{
 		k = a % 10;
 		l = k << m;
-		printf("%d\n", l);
 		a /= 10;
 		m++;
 		sum += l;
+		}
 	}
 
 	return (sum);
--- 0x14-bit_manipulation/test1.c
+++ 0x14-bit_manipulation/test1.c
@@ -12,27 +12,35 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int a = atoi(b), j, k, l, m = 0, sum = 0;;
+	int a = atoi(b), c;
+	int j, k, l;
+	int m = 0, sum = 0;
 
-	
+	for (c = 0; b[c]; c++)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return 0;
+	}
+
 	while (a)
 	{
 		k = a % 10;
+		printf("%d\n", k);
 		l = k << m;
 		printf("%d\n", l);
 		a /= 10;
 		m++;
 		sum += l;
+
 	}
 
 	return (sum);
-
 }
 
 int main(void)
 {
-	int b = binary_to_uint("1e01");
+	int b = binary_to_uint("1000001");
 
-	printf("%d\n", b);
+	printf("\n%d\n", b);
 	return (b);
 }
