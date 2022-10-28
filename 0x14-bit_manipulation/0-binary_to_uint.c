928ef800c6f279b3f7340e48225acdf17ac04c86
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-27 18:12:54 -0700

    Convert binary to decimal using bit operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,9 +8,8 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int a = 0, c, d = 0;
-	int j, k, l, m = 0;
-	unsigned int sum = 0;
+	int c;
+	unsigned int sum, exp;
 
 	if (b == NULL)
 		return (0);
@@ -18,25 +17,11 @@ unsigned int binary_to_uint(const char *b)
 	for (c = 0; b[c]; c++)
 		if (b[c] != '0' && b[c] != '1')
 			return (0);
-
-	while (b[d] != '\0')
+	for (exp = 1, sum = 0, c--; c >= 0; c--, exp *= 2)
 	{
-		if (b[d] < 48 || b[d] > 57)
-		{
-			return (0);
-		}
-		else
-		a = a * 10 + (b[d] - 48);
-		d++;
+		if (b[c] == '1')
+			sum += exp;
 	}
 
-	while (a)
-	{
-		k = a % 10;
-		l = k << m;
-		a /= 10;
-		m++;
-		sum += l;
-	}
 	return (sum);
 }
