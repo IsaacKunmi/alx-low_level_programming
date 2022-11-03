62f6d6f438d2a4eb481b5bbb6dc331f79e08f728
IsaacKunmi <kunmiolamiju@gmail.com>
2022-11-03 09:56:19 -0700

    create a file with open function in C

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,11 +12,22 @@ int create_file(const char *filename, char *text_content)
 {
 	int a, b;
 
-	a = open(filename, O_RDWR | O_CREAT, 0600);
+	if (!filename)
+		return (-1);
+
+	a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (a == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
 
 	b = read(a, text_content, sizeof(text_content));
 
 	write(a, text_content, b);
+
+	close(a);
 
 	return (1);
 }
