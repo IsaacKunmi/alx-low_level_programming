88f38e41bdbc5e4dd38ec89f739f597ee0d1fdae
IsaacKunmi <kunmiolamiju@gmail.com>
2022-11-03 11:46:16 -0700

    Append a file with open function in C

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - function appends text at the end of a file
+ *
+ *@filename: name of file
+ *@text_content: string to add
+ *Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int a, b;
+
+	if (!filename)
+		return (-1);
+
+	a = open(filename, O_WRONLY | O_APPEND);
+
+	if (a == -1)
+		return (-1);
+
+	if (!text_content)
+		return (-1);
+
+	for (b = 0; text_content[b]; b++)
+		;
+
+	write(a, text_content, b);
+
+	return (1);
+}
+
